a7bcb5088bcb68b4096be2355be3d6c33d4d13a2
Fene-87 <fenekayas87@gmail.com>
2023-06-13 12:07:57 +0300

    Create rails back end API

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,16 @@ module HelloRailsBackEnd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'  # Add the allowed origin(s) here
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'  # Add the allowed origin(s) here
     
-        resource '/api/*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head],
-          credentials: true
-      end
-    end
+    #     resource '/api/*',
+    #       headers: :any,
+    #       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    #       credentials: true
+    #   end
+    # end
 
     # Configuration for the application, engines, and railties goes here.
     #
